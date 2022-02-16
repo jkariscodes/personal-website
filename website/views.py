66f63c0d9adbecca328d8901e52700f8271b52ac
Joseph Kariuki <contact@josephkariuki.com>
2022-02-16 10:38:59 +0300

    Added views for home and blog.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+class PostView(ListView):
+    model = Post
+    template_name = 'blog.html'
+
+
+class ArticleView(DetailView):
+    model = Post
+    template_name = 'article-detail.html'
+
