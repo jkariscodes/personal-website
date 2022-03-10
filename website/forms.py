b3777367e1c771537461e42ab485db45348fdbad
Joseph Kariuki <contact@josephkariuki.com>
2022-03-10 10:14:15 +0300

    Customized forms' widgets layout.

--- website/forms.py
+++ website/forms.py
@@ -32,10 +32,10 @@ class EmailPostForm(forms.Form):
     """
     Share blog post form.
     """
-    name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    to = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
 
 
 class CommentForm(forms.ModelForm):
@@ -51,7 +51,7 @@ class ContactForm(forms.Form):
     """
     Contact form.
     """
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control'}))
 
