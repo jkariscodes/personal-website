b11cafdb10debce04b61828a2b85861d5644f6ea
Joseph Kariuki <contact@josephkariuki.com>
2022-03-18 19:17:51 +0300

    Updated static files.

--- static/website/js/main.js
+++ static/website/js/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
-   
-    $(".navbar a").click(function(){
-        $("body,html").animate({
-            scrollTop:$("#" + $(this).data('value')).offset().top
-        },1000)
-   
-    })
-   
-   })
+})
    
    
