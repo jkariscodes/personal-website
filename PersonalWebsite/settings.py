1a4c3c957f2cc36a50f076e6e645131c80b83241
Joseph Kariuki <contact@josephkariuki.com>
2022-03-19 13:44:04 +0300

    Added sample .env configuration.

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PersonalWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'website',
+       'USER': 'website_user',
+       'PASSWORD': '#7F@jQ!9Y$xO',
+       'HOST': '127.0.0.1',
+       'PORT': '5801',
     }
 }
 
