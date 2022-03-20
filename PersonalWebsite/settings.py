9301fd4c30a1ee3f14d80a3b86b207306c3d61df
Joseph Kariuki <contact@josephkariuki.com>
2022-03-20 15:06:07 +0300

    Disabled debug mode.

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = '1b^4egfaha)c1hxlop!-bgpcfptdi9qjh!oj4sp!zx*z_g+c=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
