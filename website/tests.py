6b4f1439fa08816594a4f66aa7dd21967d3439f8
Joseph Kariuki <contact@josephkariuki.com>
2022-04-04 09:40:29 +0300

    Added initial simple test cases.

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_landing_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_status_code(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_portfolio_page_status_code(self):
+        response = self.client.get('/portfolio/')
+        self.assertNotEqual(response.status_code, 400)
+
