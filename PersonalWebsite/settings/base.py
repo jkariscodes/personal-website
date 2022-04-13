7d89ddc8f19efb7a8e719a01afd35ea47f9506fa
Joseph Kariuki <joehene@gmail.com>
2022-04-13 12:13:11 +0300

    Refactored static configuration.

--- PersonalWebsite/settings/base.py
+++ PersonalWebsite/settings/base.py
@@ -120,12 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Handling images
