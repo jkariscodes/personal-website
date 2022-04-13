53c9a953ce89b7c2f48dd90be99ed426beb54919
Joseph Kariuki <joehene@gmail.com>
2022-04-13 17:20:08 +0300

    Added S3 media storage configuration.

--- PersonalWebsite/settings/pro.py
+++ PersonalWebsite/settings/pro.py
@@ -20,6 +20,8 @@ AWS_S3_CUSTOM_DOMAIN = 'your-cloudfront-domain.com'
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+DEFAULT_FILE_STORAGE = 'PersonalWebsite.settings.storage_backends.MediaStorage'  # the media storage configurations
+
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 STATICFILES_DIRS = [
