921de528dbf52a57ddd1e3e4f0bbb5bd00d09d87
Joseph Kariuki <joehene@gmail.com>
2022-04-18 05:54:54 +0300

    Added SMTP configuration.

--- PersonalWebsite/settings/pro.py
+++ PersonalWebsite/settings/pro.py
@@ -37,3 +37,12 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 django_heroku.settings(locals())
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = os.environ('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = os.environ('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
