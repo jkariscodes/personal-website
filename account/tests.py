53d903c8493ff6df4079a77b96b7d38a81b44c62
Joseph Kariuki <joehene@gmail.com>
2022-04-22 05:58:04 +0300

    Added test cases for the user creation.

--- account/tests.py
+++ account/tests.py
@@ -1,2 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.test import SimpleTestCase
+
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='jkariuki',
+            email='jkariuki@email.com',
+            password='somepassword',
+        )
+        self.assertEqual(user.username, 'jkariuki')
+        self.assertEqual(user.email, 'jkariuki@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='mzito',
+            email='mzito@email.com',
+            password='superpassword'
+        )
+        self.assertEqual(admin_user.username, 'mzito')
+        self.assertEqual(admin_user.email, 'mzito@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
