dccf38223719197a7c7b9319539778c2b35af2af
Joseph Kariuki <joehene@gmail.com>
2022-08-15 05:24:02 +0300

    Refactored database schema to allow bleach to sanitize fields without error.

--- website/migrations/0004_alter_post_body.py
+++ website/migrations/0004_alter_post_body.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('website', '0003_alter_post_body'),
+        ('website', '0002_remove_post_header_image'),
     ]
 
     operations = [
