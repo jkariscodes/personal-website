e12aca8891bd38bf7bf576894e709a5f73c2b502
Joseph Kariuki <joehene@gmail.com>
2023-03-14 16:36:13 +0300

    Added database entrypoint script.

---
+++ initdb/01-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASSWORD';
+  ALTER USER $APP_DB_USER CREATEDB;
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
+  \connect $APP_DB_NAME $APP_DB_USER
+  BEGIN;
+    CREATE TABLE IF NOT EXISTS event (
+      id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+	    aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+	    event_data JSON NOT NULL,
+	    version INT,
+	    UNIQUE(aggregate_id, version)
+    );
+    CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
+  COMMIT;
+EOSQL
