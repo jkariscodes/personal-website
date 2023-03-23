05dd86310d2672c1accc1bd3bcd1c840b20a0cd1
Joseph Kariuki <joehene@gmail.com>
2023-03-23 10:02:06 +0300

    Refactored portfolio page.

---
+++ website/templatetags/project_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from ..models import PortfolioProjects
+
+register = template.Library()
+
+
+@register.inclusion_tag("website/all_projects.html")
+def all_projects():
+    projects = PortfolioProjects.objects.all().order_by('title')
+    return {'all_projects': projects}
