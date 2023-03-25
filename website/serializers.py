e878c010a2dbdea2fbc99e5e7dd60e7fe7d66641
Joseph Kariuki <joehene@gmail.com>
2023-03-25 13:30:38 +0300

    Updated rest configurations on website app.

---
+++ website/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Post, PostComment
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Post
+        fields = ('user','title', 'body', 'published', 'updated', 'category', 'slug')
+
+
+class PostCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostComment
+        fields = ('name', 'email', 'body', 'created', 'updated')
