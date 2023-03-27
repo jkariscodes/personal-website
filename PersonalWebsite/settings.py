c2449af42942d0b7738625a8836ffdb657816b38
Joseph Kariuki <joehene@gmail.com>
2023-03-27 08:53:09 +0300

    Disabled whitenoise app.

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "whitenoise.runserver_nostatic",
+    # "whitenoise.runserver_nostatic",
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
