80552a948945838ab619d0d9b8f5b293bb92a4ea
Joseph Kariuki <joehene@gmail.com>
2023-04-23 17:32:27 +0300

    Fixes #37

--- PersonalWebsite/settings/base.py
+++ PersonalWebsite/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     # 3rd Party
     "crispy_forms",
     "crispy_bootstrap5",
