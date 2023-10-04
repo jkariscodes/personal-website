536ce45d4439cb10bc9b8dcb8c269efbe5654143
Joseph Kariuki <joehene@gmail.com>
2023-10-04 15:35:58 +0300

    Fixes #56 on django-allauth middleware

--- PersonalWebsite/settings/base.py
+++ PersonalWebsite/settings/base.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'PersonalWebsite.urls'
